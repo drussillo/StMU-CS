53c08afad12e78bf8288d018302de42f883ba906
BigDave0071 <daverussillo7@gmail.com>
2024-11-14 18:01:37 -0600

    Finished bubblesort program in c

--- Fall-2024/misc/bubblesort.c
+++ Fall-2024/misc/bubblesort.c
@@ -1,19 +1,57 @@
 /* Bubble sort in C: Test */
 #include <stdio.h>
 
-int is_sorted(int array[]) {
-    int i;
-    for(i = 0; i < sizeof(array)-1; i++) {
-        if(array[i] > array[i+1]) {
-            return 0;
-        }
+int is_sorted(int* array, int length) {
+  int i;
+  for(i = 0; i < length; i++) {
+    if(array[i] > array[i+1]) {
+        return 0;
     }
-    return 1;
+  }
+  return 1;
+}
+
+void print_array(int* array, int length) {
+  for(int i = 0; i < length; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+void fill_array(int* array, int length) {
+  int value;
+
+  for(int i = 0; i < length; i++) {
+    printf("Enter index %d value: ", i);
+    scanf("%d", &value);
+    array[i] = value;
+  }
+}
+
+void bubble_sort(int* array, int length) {
+  int temp;
+
+  while(!is_sorted(array, length)) {
+    for(int i = 0; i < length-1; i++) {
+      if(array[i] > array[i+1]) {
+        int temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+      }
+    }
+  }
 }
 
 int main(void) {
-    int nums[] = {1,2,3,4,5,6,7,8};
-    printf("%d \n", is_sorted(nums));
-    
-    return 0;
+  int length;
+  printf("Input length:  ");
+  scanf("%d", &length);
+  int nums[length];
+
+  fill_array(nums, length);
+  print_array(nums, length);
+  bubble_sort(nums, length);
+  print_array(nums, length);
+  
+  return 0;
 }
